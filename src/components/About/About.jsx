b8b02926c30e290bc0fd917e992a47e437f37e42
Ben Basinski <benbasinski97@gmail.com>
2018-12-08 20:01:37 -0800

    try new form

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,41 +43,23 @@ class About extends Component {
         <h1>
           Use the form below to get in contact!
         </h1>
-        {/* <form
-          className={classes.container}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-name"
-            label="Name"
-            className={classes.textField}
-            value={this.state.name}
-            onChange={this.handleChange('name')}
-            margin="normal"
-            variant="outlined"
-          />
-        </form> */}
-
-        <form name="contact" method="POST" netlify action="/">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          {/* <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p> */}
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/">
+          <input type="hidden" name="bot-field" />
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6"></textarea>
+          </div>
+          <ul className="actions">
+            <input type="submit" value="Send Message" className="special" />
+          </ul>
         </form>
       </div>
     );
