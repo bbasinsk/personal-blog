22dd8f229cdc2a345706fe4f0cd92e9b66d46a9c
Ben Basinski <benbasinski97@gmail.com>
2018-12-09 20:54:53 -0800

    add thanks page

---
+++ src/components/Thanks/Thanks.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Contact from '../Contact/Contact';
+
+// import "./About.css";
+
+class Thanks extends Component {
+    render() {
+        return (
+            <div className="thanks">
+                <h1>
+                    Thanks for reaching out!
+                </h1>
+            </div >
+        );
+    }
+}
+
+export default Thanks;
---
+++ src/pages/thanks.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import Thanks from "../components/Thanks/Thanks";
+import config from "../../data/SiteConfig";
+
+class ThanksPage extends Component {
+    render() {
+        return (
+            <div className="thanks-container">
+                <Helmet title={`Thanks | ${config.siteTitle}`} />
+                <Thanks />
+            </div>
+        );
+    }
+}
+
+export default ThanksPage;
